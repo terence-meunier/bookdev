25c667b71830144709a707313517b97a29e68d63
Greg_MAZA <maza.gregory46@gmail.com>
2021-02-23 17:29:25 +0100

    database: migration products table fixies #20

---
+++ database/migrations/2021_02_23_131651_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 200);
+            $table->decimal('price_ht');
+            $table->integer('weight');
+            $table->decimal('tva');
+            $table->integer('stock');
+            // $table->foreignId('categories_id');  Pour la suite clé étrangère
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
