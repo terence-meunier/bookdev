faf967fd27db5649053b8e98912f480e6ad9319f
karl <karl.hinterseber@le-campus-numerique.fr>
2021-02-25 16:35:13 +0100

    feat: create button_cart

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -20,6 +20,7 @@
         }
     </style>
 </head>
+
 <body class="antialiased">
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
     <article class="dark:text-white">
@@ -33,6 +34,7 @@
                 <li>Poids : {{ $product->weight }} g</li>
                 <li>Stock : {{ $product->stock }}</li>
             </ul>
+            <button class="button-cart" type="button">Ajouter au panier</button>
         </footer>
     </article>
 </div>
