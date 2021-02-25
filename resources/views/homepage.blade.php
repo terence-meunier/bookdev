e7d2113a4e6157e204b43f617e9a9a7351074aa1
Unknown <unknown@example.com>
2021-02-25 10:03:44 +0100

    fix(home):fix home view

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,22 @@
-<html>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Accueil</title>
+</head>
 <body>
-@foreach ($products as $product)
-    <p>This is product {{ $product->title }}</p>
-@endforeach
+<div>
+    @foreach ($products as $product)
+        <article>
+            <header>
+                <h2>{{ $product->title }}</h2>
+            </header>
+            <p>{{ $product->description }}</p>
+        </article>
+    @endforeach
+</div>
 </body>
 </html>
