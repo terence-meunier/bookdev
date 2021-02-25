61886a7f75aabb8ba850af5cbd4490bfaf8255c5
Unknown <unknown@example.com>
2021-02-25 12:08:49 +0100

    feat(categories): Add and Call view for one category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    
+    public function show($id) {
+        $category = Category::findOrFail($id);
+        return view('categories.show', ['category' => $category]);
+    }
 }
