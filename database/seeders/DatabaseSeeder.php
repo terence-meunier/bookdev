f871e035a47bd4671516c708af10081dd17fb428
hinterseber <77095696+karl-hin@users.noreply.github.com>
2021-02-26 10:59:28 +0100

    fix: update databaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // La fonction call() lance les fonctions run() dans toutes les classes Seeder spécifiés
         $this->call([
             CategorySeeder::class,
-            ProductSeeder::classstateless
+            ProductSeeder::class
         ]);
     }
 }
