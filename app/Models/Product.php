d9e83a5b8700bb2f18ee42db4e453897900115d2
Sarah Hans <sarah@Sarahui-MacBookAir.local>
2021-03-01 11:59:35 +0100

    feat : adding comments

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
+    //Calcul de la TVA, ici à 20%
     private static $facteur_tva = 1.2;
 
     public function categories()
@@ -16,6 +17,7 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
+    //méthode qui retourne le prix ttc
     public function priceTTC() {
         $price_ttc = $this->price_ht * self::$facteur_tva;
         return number_format($price_ttc, 2);
