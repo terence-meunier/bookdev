2099acd60dda54abae6fe9ae2c0683e380fd1656
Unknown <unknown@example.com>
2021-03-01 16:24:39 +0100

    fix(cart): Cart view

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -6,32 +6,28 @@
 </div>
 @if(!empty($products))
     <div class="dark:text-white dark:bg-gray-800 text-center">
-        <form action="{{ route('cart.update') }}" method="post">
-            @csrf
-            @method('PUT')
-            <table width="100%">
-                <thead>
+        <table width="100%">
+            <thead>
+            <tr>
+                <td>#</td>
+                <td>Title</td>
+                <td>Qté</td>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($products as $id => $product)
                 <tr>
-                    <td>#</td>
-                    <td>Title</td>
-                    <td>Qté</td>
+                    <td>{{ $id }}</td>
+                    <td>{{ $product->title }}</td>
+                    <td>{{ session('cart')[$id] }}</td>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach ($products as $id => $product)
-                    <tr>
-                        <td>{{ $id }}</td>
-                        <td>{{ $product->title }}</td>
-                        <td><input type="number" name="{{ $id }}" value="{{ session('cart')[$id] }}" min="1"/></td>
-                    </tr>
-                @endforeach
-                <tr>
-                    <td colspan="2"></td>
-                    <td><input type="submit" value="Modifier les quantités"/></td>
-                </tr>
-                </tbody>
-            </table>
-        </form>
+            @endforeach
+            <tr>
+                <td colspan="2"></td>
+                <td><input type="submit" value="Modifier les quantités"/></td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 @else
     <div>
