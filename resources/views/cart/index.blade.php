dae92dfacaef901e76fb08d214182712193cf1c1
Unknown <unknown@example.com>
2021-03-01 12:22:25 +0100

    feat(Cart):Update Cart main view

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,26 +4,40 @@
 <div class="dark:text-white dark:bg-gray-800 text-center">
     <h2>Votre panier</h2>
 </div>
-<div class="dark:text-white dark:bg-gray-800 text-center">
-    <table width="100%">
-        <thead>
-        <tr>
-            <td>#</td>
-            <td>Title</td>
-            <td>Qté</td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($products as $id => $product)
-            <tr>
-                <td>{{ $id }}</td>
-                <td>{{ $product->title }}</td>
-                <td>{{ session('cart')[$id] }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+@if(!empty($products))
+    <div class="dark:text-white dark:bg-gray-800 text-center">
+        <form action="{{ route('cart.update') }}" method="post">
+            @csrf
+            @method('PUT')
+            <table width="100%">
+                <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Title</td>
+                    <td>Qté</td>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($products as $id => $product)
+                    <tr>
+                        <td>{{ $id }}</td>
+                        <td>{{ $product->title }}</td>
+                        <td><input type="number" name="{{ $id }}" value="{{ session('cart')[$id] }}" min="1"/></td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td colspan="2"></td>
+                    <td><input type="submit" value="Modifier les quantités"/></td>
+                </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+@else
+    <div>
+        <p>Il n'y a pas d'articles dans le panier</p>
+    </div>
+@endif
 </body>
 
 @include ('layouts.footer')
