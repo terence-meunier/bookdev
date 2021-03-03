67fd39886d33d32af2b16eff53b7c32f4c1b84d6
Térence Meunier <terence.meunier@gmail.com>
2021-03-03 15:31:34 +0100

    feat(dashboard):view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-white bg-gray-800 leading-tight p-4">
+            {{ __('Tableau de bord') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+            <div class="overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="grid p-6 border-b border-gray-200 place-items-center">
+                    <a href="{{ route('home') }}"><img src="/img/bookdev-logo.png"/></a>
+                </div>
+                <div class="grid p-6 border-b border-gray-200 place-items-center">
+                    <a href="{{ route('home') }}"><button class="bg-white text-gray-900 hover:text-yellow-600 p-8 rounded-2xl">ENTRER SUR LE SITE</button></a>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-800">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
@@ -28,7 +28,7 @@
             </header>
 
             <!-- Page Content -->
-            <main>
+            <main class="text-white">
                 {{ $slot }}
             </main>
         </div>
