2443feaac606fbd5ff3e11291f11b0560f42e991
Unknown <unknown@example.com>
2021-03-05 10:37:10 +0100

    feat(orders): Format date orders in view

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,18 +1,15 @@
 @include ('layouts.header')
 
-
-
     <h2>Mes Commandes</h2>
     <ul>
         @foreach ($orders as $order)
             <li><a href="{{ route('orders.show', ['order' => $order]) }}"><b>Commande N°: {{ $order->number }}</b></a></li>
-            <li> Date de la commande: {{$order->order_create}}</li>
-            <li> Date de livraison: {{$order->order_delivery}}</li>
+            <li> Date de la commande: {{$order->order_create->format('d/m/Y')}}</li>
+            <li> Date de livraison: {{$order->order_delivery->format('d/m/Y')}}</li>
             <br>
         @endforeach
     </ul>
 </div>
 </body>
-
 
 @include ('layouts.footer')
